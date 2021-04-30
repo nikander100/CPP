04049041a9932ab649fb0cdd57f000c27620ba4f
nikander100 <nvan-der@student.codam.nl>
2021-04-30 09:08:11 +0200

    Intro: Init_list

---
+++ d00/Intro/init_list/Sample2.class.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Sample2.class.hpp"
+
+Sample2::Sample2(char p1, int p2, float p3) :
+	a1(p1), a2(p2), a3(p3) //init list a1 = p1, a2 = p2, etc.
+{
+	std::cout << "Constructor called" << std::endl;
+	std::cout << "this->a1 = " << this->a1 << std::endl;
+	std::cout << "this->a2 = " << this->a2 << std::endl;
+	std::cout << "this->a3 = " << this->a3 << std::endl;
+}
+
+Sample2::~Sample2(void)
+{
+	std::cout << "Deconstructor called" << std::endl;
+	return;
+}
