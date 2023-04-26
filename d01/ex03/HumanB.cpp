f70485ef983740f07760538b302cf8f187223a31
Nikander van der Schee <nvan-der@student.codam.nl>
2023-04-26 19:41:40 +0200

    Update HumanB.cpp

--- d01/ex03/HumanB.cpp
+++ d01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: nvan-der <nvan-der@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/26 18:18:36 by nvan-der      #+#    #+#                 */
-/*   Updated: 2023/04/26 19:40:25 by nvan-der      ########   odam.nl         */
+/*   Updated: 2023/04/26 19:41:35 by nvan-der      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,10 @@ HumanB::~HumanB(void) {}
 
 void HumanB::attack(void) {
 	std::cout << _name << " attacks with ";
-	if (_weapon != NULL) {
+	if (_weapon != NULL)
 		std::cout << "their " << _weapon->getType() << std::endl; 
-	} else {
+	else
 		std::cout << "their fists!!" << std::endl; 
-	}
 }
 void HumanB::setWeapon(Weapon& weapon) {
 	_weapon = &weapon;
