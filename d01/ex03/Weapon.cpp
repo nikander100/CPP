17f7c4ac79e14aa01c967b17754e0c272403e42e
Nikander van der Schee <nvan-der@student.codam.nl>
2023-05-11 17:27:25 +0200

    type change to weapon private _type

--- d01/ex03/Weapon.cpp
+++ d01/ex03/Weapon.cpp
@@ -6,15 +6,15 @@
 /*   By: nvan-der <nvan-der@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/26 18:18:31 by nvan-der      #+#    #+#                 */
-/*   Updated: 2023/05/11 17:17:16 by nvan-der      ########   odam.nl         */
+/*   Updated: 2023/05/11 17:26:53 by nvan-der      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
 
-Weapon::Weapon(void): _type(std::string("default weapon")) {}
+Weapon::Weapon(void): _type("default weapon") {}
 
-Weapon::Weapon(const char *type): _type(std::string(type)) {}
+Weapon::Weapon(const std::string &type): _type(type) {}
 
 Weapon::~Weapon(void) {}
 
@@ -22,6 +22,6 @@ const std::string &Weapon::getType(void) const {
 	return _type;
 }
 
-void Weapon::setType(const char *type) {
-	_type = std::string(type);
+void Weapon::setType(const std::string &type) {
+	_type = (type);
 }
--- d01/ex03/Weapon.hpp
+++ d01/ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: nvan-der <nvan-der@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/26 18:18:41 by nvan-der      #+#    #+#                 */
-/*   Updated: 2023/05/11 17:16:17 by nvan-der      ########   odam.nl         */
+/*   Updated: 2023/05/11 17:26:40 by nvan-der      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ class Weapon {
 	public:
 
 	Weapon(void);
-	Weapon(const char *type);
+	Weapon(const std::string &type);
 	~Weapon(void);
 
 	const	std::string &getType(void) const;
-	void	setType(const char *type);
+	void	setType(const std::string &type);
 
 	
 	private:
