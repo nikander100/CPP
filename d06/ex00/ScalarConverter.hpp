56356cc9a9289a7724a655c6227e9bbbe3787bd3
Nikander van der Schee <nikander@student.codam.nl>
2023-11-17 17:58:35 +0100

    start d06ex00

---
+++ d06/ex00/ScalarConverter.hpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ScalarConverter.hpp                                :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nvan-der <nvan-der@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/11/17 17:18:55 by nvan-der      #+#    #+#                 */
+/*   Updated: 2023/11/17 17:56:22 by nvan-der      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <string>
+#include <exception>
+#include <cmath>
+#include <cctype>
+#include <limits>
+
+class ScalarConverter {
+public:
+	ScalarConverter(const std::string &input);
+	ScalarConverter(const ScalarConverter &copy);
+	virtual ~ScalarConverter();
+
+	ScalarConverter &operator=(const ScalarConverter &rhs);
+	operator int() const;
+	operator char() const;
+	operator float() const;
+	operator double() const;
+
+	class NonDisplayableException : public std::exception {
+	public:
+		virtual const char* what() const throw();
+	};
+
+	class ImpossibleException : public std::exception {
+	public:
+		virtual const char* what() const throw();
+	};
+
+	class InvalidInputException : public std::exception {
+	public:
+		virtual const char* what() const throw();
+	};
+	
+private:
+	bool isNumber(const std::string &input);
+	bool num;
+	double intVal;
+	ScalarConverter();
+};
+
+std::ostream &operator<<(std::ostream &out, ScalarConverter const &rhs);
