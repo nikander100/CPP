4bc62b6f750d628135cc850a31bcbb1fd713634c
Nikander van der Schee <nikander@student.codam.nl>
2023-11-22 20:42:46 +0100

    progress but having problems with floats and +-inf and inff

--- d06/ex00/ScalarConverter.cpp
+++ d06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: nvan-der <nvan-der@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/17 17:18:52 by nvan-der      #+#    #+#                 */
-/*   Updated: 2023/11/21 20:10:48 by nvan-der      ########   odam.nl         */
+/*   Updated: 2023/11/22 20:42:10 by nvan-der      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,11 +48,40 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &rhs) {
 }
 
 ScalarConverter::operator int() const {
-	double converted = static_cast<double>(*this);
+	double convertedInt = static_cast<double>(*this);
 
-	if (std::isnan(converted) || !(std::numeric_limits<int>::min() <= converted && converted <= std::numeric_limits<int>::max()))
+	if (std::isnan(convertedInt) || !(std::numeric_limits<int>::min() <= convertedInt && convertedInt <= std::numeric_limits<int>::max()))
 		throw ImpossibleException();
-	return static_cast<int>(converted);
+	return static_cast<int>(convertedInt);
+}
+
+ScalarConverter::operator char() const {
+	int convertedChar = static_cast<int>(*this);
+
+	if (convertedChar <= std::numeric_limits<char>::min() || convertedChar >= std::numeric_limits<char>::max())
+		throw ImpossibleException();
+	if (std::isprint(convertedChar))
+		return static_cast<char>(convertedChar);
+	throw NonDisplayableException();
+}
+
+ScalarConverter::operator float() const {
+	double convertedFloat = static_cast<double>(*this);
+
+	return static_cast<float>(convertedFloat);
+}
+
+ScalarConverter::operator double() const {
+	if (std::isinf(_intVal))
+		throw ImpossibleException();
+	return _intVal;
+}
+
+bool ScalarConverter::isNumber(const std::string& s) {
+	std::string::const_iterator it = s.begin();
+	// while ((it != s.end() && std::isdigit(*it)) || *it == '.') ++it;
+	while ((it != s.end() && std::isdigit(*it))) ++it;
+	return !s.empty() && it == s.end();
 }
 
 // Exceptions
@@ -69,9 +98,34 @@ const char* ScalarConverter::InvalidInputException::what() const throw() {
 	return "Invalid input";
 }
 
-static bool isNumber(const std::string& s)
-{
-	std::string::const_iterator it = s.begin();
-	while (it != s.end() && std::isdigit(*it)) ++it;
-	return !s.empty() && it == s.end();
+std::ostream &operator<<(std::ostream &out, ScalarConverter const &rhs) {
+	try {
+		out << "  char: ";
+		out << static_cast<char>(rhs);
+	} catch (const std::exception &e) {
+		out << e.what();
+	}
+	out << std::endl;
+	try {
+		out << "   int: ";
+		out << static_cast<int>(rhs);
+	} catch (const std::exception &e) {
+		out << e.what();
+	}
+	out << std::endl;
+	try {
+		out << " float: ";
+		out << static_cast<float>(rhs);
+	} catch (const std::exception &e) {
+		out << e.what();
+	}
+	out << "f" << std::endl;
+	try {
+		out << "double: ";
+		out << static_cast<double>(rhs);
+	} catch (const std::exception &e) {
+		out << e.what();
+	}
+	out << std::endl;
+	return out;
 }
